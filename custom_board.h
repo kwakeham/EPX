996df79f53a6111ed8d645632a6fd8ca7e392cd1
Keith <kwakeham@gmail.com>
2023-08-12 16:10:43 -0600

    upate board pinout

--- custom_board.h
+++ custom_board.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 - 2022, TITAN LAB Inc.
+ * Copyright (c) 2019 - 2023, TITAN LAB Inc.
  *
  */
 #ifndef CUSTOM_BOARD_H
@@ -16,35 +16,36 @@ extern "C" {
 
 //TEST
 
-#define MAX_DRDY 2
-#define MAX_SCK_PIN  4
-#define MAX_MISO_PIN 25
-#define MAX_nRST 26
-#define MAX_SS_PIN 27
-#define MAX_MOSI_PIN 30
+#define M_EN 6
+#define M_Phase 7
+#define M_Mode 8
+#define M_nSleep 9
+#define M_nFault 5
 
-#define MXC_SDA 5
-#define MXC_SCL 7
+#define M_ISENSE    3 //AIN1
 
-#define CRM_SCLK 6
-#define CRM_CS 8
+#define S_HALL_EN   31
+#define S_SIN       4 //AIN2
+#define S_COS       2 //AIN0
 
-#define CRM_DIN 10
-#define CRM_DOUT 11
-#define CRN_nRST 12
 
-#define LSM6DSM_CS 13
-#define LSM6DSM_SCLK 15
-#define LSM6DSM_MISO 16
-#define LSM6DSM_MOSI 17
-#define LSM6DSM_INT1 18
+#define BATT_SDA 14
+#define BATT_SCL 10
+
+
+#define LIS2DTW_CS 17
+#define LIS2DTW_SCLK 18
+#define LIS2DTW_SDI 15
+#define LIS2DTW_SDO 16
+#define LIS2DTW_INT1 19
 
 #define GAGE_CTRL 19
 
-#define TX_PIN_NUMBER 20
+#define TX_PIN_NUMBER 23
 
-#define TP1 9
-#define TP2 14
+#define TP1 27
+#define TP2 28
+
 #define nRST 21
 
 #define MAG_DIGITAL 22
@@ -54,19 +55,20 @@ extern "C" {
 #define LEG_R 24
 
 
+#define BUTTONS_NUMBER 3 //because not in order? TODO: Check this
+#define BUTTON_1 20
+#define BUTTON_2 13
+#define BUTTON_3 12
+#define BUTTON_4 11
 
-
-#define BUTTONS_NUMBER 2
-#define BUTTON_1 TP1
-#define BUTTON_2 TP2
-
-#define BSP_BUTTON_0            BUTTON_1
-#define BSP_BUTTON_1            BUTTON_2
+#define BSP_BUTTON_0            BUTTON_2
+#define BSP_BUTTON_1            BUTTON_3
+#define BSP_BUTTON_2            BUTTON_4
 
 #define BUTTON_PULL             NRF_GPIO_PIN_PULLUP
 #define BUTTONS_ACTIVE_STATE    0
 
-#define BUTTONS_LIST            { BUTTON_1 , BUTTON_2}
+#define BUTTONS_LIST            { BUTTON_2, BUTTON_3, BUTTON_4}
 
 #ifdef __cplusplus
 }
