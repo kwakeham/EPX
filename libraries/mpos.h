c57c0c50c6e3864544cd015ae870232009e7229d
Keith <kwakeham@gmail.com>
2023-08-12 20:18:37 -0600

    past the actual header, opps forgot

---
+++ libraries/mpos.h
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2018-2023 Titan Lab Inc.
+ *
+ * All rights reserved.
+ *
+ *
+ */
+
+#ifndef MPOS_H
+#define MPOS_H
+
+#include <stdint.h>
+#include <math.h>
+#include <inttypes.h>
+#include "nrfx_saadc.h"
+#include "app_error.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void saadc_callback(nrfx_saadc_evt_t const * p_event);
+
+void steer_init(void);
+
+int16_t mpos_test_convert(void);
+
+void mpos_test_convert_event_activate(void);
+
+double angle(int16_t hall_0, int16_t hall_1);
+
+void display_value(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MPOS_H
