8605a3292f9aefee9fae2c82a2b7db5328b3a619
Keith <kwakeham@gmail.com>
2023-08-17 19:43:48 -0600

    oh derp, I wrote it in the wrong file, flip

--- libraries/PID_controller.h
+++ libraries/PID_controller.h
@@ -12,44 +12,6 @@
 #ifndef PID_CONTROLLER__
 #define PID_CONTROLLER__
 
-// PID Controller parameters
-#define Kp 1.0   // Proportional gain
-#define Ki 0.1   // Integral gain
-#define Kd 0.01  // Derivative gain
-
-// PID Controller state variables
-double previousError = 0.0;
-double integral = 0.0;
-
-// Integral limits
-double integralMin = -10.0;
-double integralMax = 10.0;
-
-// PID Controller function
-float pidController(float setpoint, float measuredValue) {
-    // Calculate the error
-    float error = setpoint - measuredValue;
-    
-    // Calculate the integral term
-    integral += error;
-    
-    // Apply integral limits
-    if (integral > integralMax) {
-        integral = integralMax;
-    } else if (integral < integralMin) {
-        integral = integralMin;
-    }
-    
-    // Calculate the derivative term
-    float derivative = error - previousError;
-    
-    // Calculate the control signal
-    float controlSignal = Kp * error + Ki * integral + Kd * derivative;
-    
-    // Update state variables
-    previousError = error;
-    
-    return controlSignal;
-}
+float pidController(float setpoint, float measuredValue);
 
 #endif
