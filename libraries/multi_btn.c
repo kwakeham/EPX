510aaadb7db39d164420641a90f799fa30195631
Keith <kwakeham@gmail.com>
2024-01-20 11:01:49 -0700

    add logging registration, switch info to debug

--- libraries/multi_btn.c
+++ libraries/multi_btn.c
@@ -17,24 +17,33 @@
 #include "app_timer.h"
 #include "app_button.h"
 #include "boards.h"
-#include "nrf_log.h"
+// #include "nrf_log.h"
 #include "sdk_config.h"
 #include "multi_btn.h"
 #include "app_util_platform.h"
 
 
+
+#define NRF_LOG_MODULE_NAME multibut
+#define NRF_LOG_LEVEL       3
+#define NRF_LOG_INFO_COLOR  0
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+//Button intervals
 #define LONGPRESS_INTERVAL_MS        500UL
 #define LONGPRESS_TIMER_INTERVAL_MS        10UL //10ms makes it simple
 #define LONGPRESS_TICKS LONGPRESS_INTERVAL_MS/LONGPRESS_TIMER_INTERVAL_MS
 #define DEBOUNCE_TIME_MS        20UL
 
-//Buttons defined via SDK, to change
+//Buttons defined via SDK in the customboard.h, to change
 #define MULTI_BTN_PIN_CH1      BUTTON_1
 #define MULTI_BTN_PIN_CH2      BUTTON_2
 #define MULTI_BTN_PIN_CH3      BUTTON_3
 #define MULTI_BTN_PIN_CH4      BUTTON_4
 
-//Pullup will be 10 - 15k, this is okay for some stuff but not others
+//Pullup will be 10 - 15k, this is okay for some stuff but not others.
+//For this it's fine but it means 0.2ma drain per button, that's kinda meaningful for coin but less for this
 static app_button_cfg_t  button_cfg[BUTTONS_NUMBER]= {
 	{MULTI_BTN_PIN_CH1, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, button_callback}, 
 	{MULTI_BTN_PIN_CH2, APP_BUTTON_ACTIVE_LOW, NRF_GPIO_PIN_PULLUP, button_callback},
@@ -49,7 +58,7 @@ static multibtn_event_callback_t   m_registered_callback = &_default_callback;
 //static app_button_cfg_t  button_cfg;
 static uint8_t btn_hold_count[4] = {0,0,0,0};
 static bool button_pressed[4] = {0,0,0,0};
-static bool run_long_timer = false; //Should we run the timer for checking longs
+static bool run_long_timer = false; //Should we run the timer for checking longs NOTE: I think this can be removed
 
 static uint8_t is_button_init=0;
 
@@ -73,7 +82,8 @@ void button_timeout_handler (void * p_context)
 			if(btn_hold_count[i]>(LONGPRESS_TICKS))
 			{
 				btn_hold_count[i] -= LONGPRESS_TICKS;
-				NRF_LOG_INFO("LONG PRESS CH%d",(i+1));
+				NRF_LOG_DEBUG("LONG PRESS CH%d",(i+1));
+				m_registered_callback(MULTI_BTN_EVENT_CH1_LONG + i); //
 			}
 
 		} else
@@ -95,26 +105,26 @@ void button_callback(uint8_t pin_no, uint8_t button_action)
 
 			case MULTI_BTN_PIN_CH1:
 				button_pressed[0] = true;
-				// m_registered_callback(MULTI_BTN_EVENT_CH1_PUSH);
-				NRF_LOG_INFO("CH1 Press");
+				m_registered_callback(MULTI_BTN_EVENT_CH1_PUSH);
+				NRF_LOG_DEBUG("CH1 Press");
 				break;
 
 			case MULTI_BTN_PIN_CH2:
 				button_pressed[1] = true;
-				// m_registered_callback(MULTI_BTN_EVENT_CH2_PUSH);
-				NRF_LOG_INFO("CH2 Press");
+				m_registered_callback(MULTI_BTN_EVENT_CH2_PUSH);
+				NRF_LOG_DEBUG("CH2 Press");
 				break;
 
 			case MULTI_BTN_PIN_CH3:
 				button_pressed[2] = true;
-				// m_registered_callback(MULTI_BTN_EVENT_CH3_PUSH);
-				NRF_LOG_INFO("CH3 Press");
+				m_registered_callback(MULTI_BTN_EVENT_CH3_PUSH);
+				NRF_LOG_DEBUG("CH3 Press");
 				break;
 
 			case MULTI_BTN_PIN_CH4:
 				button_pressed[3] = true;
-				// m_registered_callback(MULTI_BTN_EVENT_CH4_PUSH);
-				NRF_LOG_INFO("CH4 Press");
+				m_registered_callback(MULTI_BTN_EVENT_CH4_PUSH);
+				NRF_LOG_DEBUG("CH4 Press");
 				break;
 
 			default:
@@ -128,26 +138,26 @@ void button_callback(uint8_t pin_no, uint8_t button_action)
 
 			case MULTI_BTN_PIN_CH1:
 				button_pressed[0] = false;
-				// m_registered_callback(MULTI_BTN_EVENT_CH1_PUSH);
-				NRF_LOG_INFO("CH1 Released");
+				m_registered_callback(MULTI_BTN_EVENT_CH1_PUSH);
+				NRF_LOG_DEBUG("CH1 Released");
 				break;
 
 			case MULTI_BTN_PIN_CH2:
 				button_pressed[1] = false;
-				// m_registered_callback(MULTI_BTN_EVENT_CH2_PUSH);
-				NRF_LOG_INFO("CH2 Released");
+				m_registered_callback(MULTI_BTN_EVENT_CH2_PUSH);
+				NRF_LOG_DEBUG("CH2 Released");
 				break;
 
 			case MULTI_BTN_PIN_CH3:
 				button_pressed[2] = false;
-				// m_registered_callback(MULTI_BTN_EVENT_CH3_PUSH);
-				NRF_LOG_INFO("CH3 Released");
+				m_registered_callback(MULTI_BTN_EVENT_CH3_PUSH);
+				NRF_LOG_DEBUG("CH3 Released");
 				break;
 
 			case MULTI_BTN_PIN_CH4:
 				button_pressed[3] = false;
-				// m_registered_callback(MULTI_BTN_EVENT_CH4_PUSH);
-				NRF_LOG_INFO("CH4 Released");
+				m_registered_callback(MULTI_BTN_EVENT_CH4_PUSH);
+				NRF_LOG_DEBUG("CH4 Released");
 				break;
 
 			default:
@@ -194,7 +204,7 @@ void multi_reset_buttons(void)
 		button_pressed[i] = 0;
 	}
 	run_long_timer = false; //clear flag
-	app_timer_stop(m_button_timer); 
+	app_timer_stop(m_button_timer);
 	m_button_timer_running = false;
 }
 
@@ -203,13 +213,13 @@ void multi_button_timer_run_check(void)
 	ret_code_t err_code;
 	if(!button_pressed[0] && !button_pressed[1] && !button_pressed[2] && !button_pressed[3])//if all buttons are not pressed...
 	{
-		NRF_LOG_INFO("no buttons multi_reset_buttons()");
+		NRF_LOG_DEBUG("no buttons multi_reset_buttons()");
 		multi_reset_buttons(); //reset everything, it'll overwrite button_pressed but that's fine, a wasted few clocks
 	} else
 	{
 		if (!m_button_timer_running) //if the timer isn't runnings
 		{	
-			NRF_LOG_INFO("timerrun");
+			NRF_LOG_DEBUG("timerrun");
 			m_button_timer_running = true;
 			err_code = app_timer_start(m_button_timer, APP_TIMER_TICKS(LONGPRESS_TIMER_INTERVAL_MS), NULL); //This starts timer to check if Long press is active
 			APP_ERROR_CHECK(err_code);
